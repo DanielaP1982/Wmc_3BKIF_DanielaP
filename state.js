2ac77d6ece7eceffa21723e59caf257576665654
DanielaP1982 <PET250723@spengergasse.at>
2026-06-22 00:18:12 +0200

    Add functions to save and retrieve step states

--- state.js
+++ state.js
@@ -1,1 +1,10 @@
+export function getSavedStates() {
+    const states = localStorage.getItem('shuffle_steps');
+    return states ? JSON.parse(states) : {};
+}
 
+export function saveStepState(stepId, isLearned) {
+    const states = getSavedStates();
+    states[stepId] = isLearned;
+    localStorage.setItem('shuffle_steps', JSON.stringify(states));
+}
